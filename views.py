86a3cbc7a20f82e15b1cf846ed35a9803b887d11
cplum <cplum@udel.edu>
2019-10-20 00:17:00 -0400

    Update

--- views.py
+++ views.py
@@ -11,13 +11,14 @@ def hello(world: dict) -> str:
 
 @simple_route('/goto/<where>/')
 def open_door(world: dict, where: str) -> str:
+    food_list = get_current_foods(world)
     if where == 'takeout':
         text = Markup('''<p>CR ran out of boxes</p>
     <p>While the box-stocking man is being beaten in the back, you decide that you will stay to dine in</p>
     <a href="/goto/dinein">Grab a plate</a>''')
         return render_template("heading.html", code=text)
     elif where == 'dinein':
-        return render_template('get_food.html')
+        return render_template('get_food.html', foods=Markup(food_list))
 
 @simple_route('/search_food/<food>/')
 def search_food(world: dict, food: str):
@@ -31,38 +32,49 @@ def search_food(world: dict, food: str):
 @simple_route('/add_to_collection/<food>')
 def add_to_collection(world: dict, food: str):
     world['foods'].append(food)
+    food_list = get_current_foods(world)
     message = '''
     <p>Success! Do you want to add another food?</p>
     <button type='button' onclick="window.location.href = '/goto/dinein/'" >Yes</button><br><button type='button' onclick="window.location.href = '/end/'" >No</button> 
     '''
-    return render_template('heading.html', code=Markup(message))
+    return render_template('current_foods.html', code=Markup(message), foods=Markup(food_list))
 
 @simple_route('/do_swap_foods/<food>')
 def do_swap_foods(world: dict, food: str):
-    return render_template("swap_foods.html", food=food, food1=world['foods'][0], food2=world['foods'][1], food3=world['foods'][2])
+    food_list = get_current_foods(world)
+    return render_template("swap_foods.html", food=food, food1=world['foods'][0], food2=world['foods'][1], food3=world['foods'][2], foods=Markup(food_list))
 
 @simple_route('/swap_foods/<new_food>')
 def swap_foods(world: dict, *args, new_food: str=''):
     world['foods'][int(request.values.get('food'))] = new_food
+    food_list = get_current_foods(world)
     message = '''
         <p>Success! Do you want to add another food?</p>
         <button type='button' onclick="window.location.href = '/goto/dinein/'" >Yes</button><br><button type='button' onclick="window.location.href = '/end/'" >No</button> 
         '''
-    return render_template('heading.html', code=Markup(message))
-
+    return render_template('current_foods.html', code=Markup(message), foods=Markup(food_list))
 
 @simple_route('/end/')
 def end(world: dict):
-    pass
+    food_list = get_current_foods(world)
+    return render_template('ending.html', foods=Markup(food_list), food1=world['foods'][0], food2=world['foods'][1], food3=world['foods'][2])
 
 def choose_food(world: dict, possible_foods: list):
+    food_list = get_current_foods(world)
     html = '''<form>
     <select id='food_select'>'''
     for food in possible_foods:
         html = html + '''<option value="{food}">{food}</option>'''.format(food=food)
     if len(world['foods']) >= 3:
         html = html + '</select><button type="button" onclick="too_many()">Select</button></form>'
-        return render_template("food_list.html", list=Markup(html))
+        return render_template("food_list.html", list=Markup(html), foods=Markup(food_list))
     elif 0 <= len(world['foods']) < 3:
         html = html + '</select><button type="button" onclick="select()">Select</button></form>'
-        return render_template("food_list.html", list=Markup(html))
+        return render_template("food_list.html", list=Markup(html), foods=Markup(food_list))
+
+def get_current_foods(world: dict):
+    food_list = "<ul>"
+    for food in world['foods']:
+        food_list = food_list + '<li>' + food + '</li>'
+    food_list = food_list + '</ul><hr>'
+    return food_list
